ce828c38047e7a6a13fddb76cdbab204bff805a1
DevSP <contatodevsp@gmail.com>
2021-02-04 15:13:24 -0300

    Aplicação Funcional

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@
 const express = require('express')
 
 
+// Importando pacote que gera IDs unicos
+const { v4: uuid } = require('uuid');
+
+
 // Criando um servidor
 const app = express()
+
+
+// Configurando o express para receber JSON nas requisições
+app.use(express.json())
 
 
 /*
@@ -29,47 +37,64 @@ const app = express()
 */
 
 
+// Emulando um banco de dados para praticar
+const projects = []
+
+
 // Criando as rotas
 app.get('/', (request, response) => {
     return response.send('Hello World')
 })
 
 app.get('/projects', (request, response) => {
-    const query = request.query
+    const { title } = request.query
 
-    console.log(query)
+    const results = title
+        ? projects.filter(project => project.title.includes(title))
+        : projects
 
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-    ])
+    return response.json(results)
 })
 
 app.post('/projects', (request, response) => {
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3',
-    ])
+    const { title, owner } = request.body
+
+    const project = { id: uuid(), title, owner }
+
+    projects.push(project)
+
+    return response.json(project)
 })
 
 app.put('/projects/:id', (request, response) => {
-    const params = request.params
+    const { id } = request.params
+    const { title, owner } = request.body
 
-    console.log(params)
+    const projectIndex = projects.findIndex(project => project.id === id)
 
-    return response.json([
-        'Projeto 4',
-        'Projeto 2',
-        'Projeto 3',
-    ])
+    if (projectIndex < 0) {
+        return response.status(400).json({ error: 'Project not found.' })
+    }
+
+    const project = { id, title, owner }
+
+    projects[projectIndex] = project
+
+    return response.json(project)
 })
 
 app.delete('/projects/:id', (request, response) => {
-    return response.json([
-        'Projeto 2',
-        'Projeto 3',
-    ])
+    const { id } = request.params
+
+    const projectIndex = projects.findIndex(project => project.id === id)
+
+    if (projectIndex < 0) {
+        return response.status(400).json({ error: 'Project not found.' })
+    }
+
+    projects.splice(projectIndex, 1)
+
+    return response.status(204).send()
 })
 
 
